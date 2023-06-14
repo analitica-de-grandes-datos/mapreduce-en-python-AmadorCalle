fbe4781a2434cd3db7d91fa37ba8bd2d4ee420af
AmadorCalle <130915431+AmadorCalle@users.noreply.github.com>
2023-06-14 06:58:44 -0500

    Update mapper.py

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        columns = line.split(',')
+        sys.stdout.write("{}\t{}\n".format(columns[3], columns[4]))
